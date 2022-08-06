9785948460f69563ee11a97898148ed9c2550b82
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-06 10:05:58 +0530

    first commit

---
+++ src/Demo/NameWord.java
@@ -0,0 +1,56 @@
+package Demo;
+
+public class NameWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+//		System.out.println("Hi I am Saiprasad Patil");
+//		System.out.println("I am from Kolhapur");
+//		System.out.println("I am learning Software Testing Course");
+		
+//		Int a,b,c;
+//		a=15;
+//		b=65;
+//		c=a+b;
+//		System.out.println("Sum of two Numbers"+c);
+		
+//		int a,b,k;
+//		a=95;
+//		b=65;
+//		k=a*b;
+//		
+//		System.out.println("Multiplication of 95 & 65 is (95*65):" +k);
+		
+//		int a,b,c,d,e,f,g;
+//		a=58;
+//		b=95;
+//		c=65;
+//		d=32;
+//		e=120;
+//		f=987;
+//		g=(a+b)*c/d-e*f;
+//		System.out.println("Solution of Mathematical line"+g);
+		
+		
+//        Short x=25001;
+//        System.out.println("Show the given Number is (25001):"+x);
+		
+//		Float b=136542.12365f;
+//		System.out.println("Detect the Decimal Number As per given Data that is (136542.123):"+b);
+		
+		
+//		System.out.println("Saiprasad Subhash Patil");
+//		System.out.println("I am from Kolhapur");
+//		System.out.println("I am Civil Engineer");
+
+	
+	
+		
+				
+	
+		
+	}
+
+}
